c6316ccd3c1853b76f7a75380633932ba365c334
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-14 12:30:52 +0200

    implmented auto increment ids in map

--- pet-clinic-web/src/main/java/pl/mk/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/mk/sfgpetclinic/bootstrap/DataLoader.java
@@ -16,9 +16,9 @@ import pl.mk.sfgpetclinic.services.VetService;
 public class DataLoader implements CommandLineRunner {
 
 
-    private final OwnerService ownerService;
+    private OwnerService ownerService;
     //private final PetService petService;
-    private final VetService vetService;
+    private VetService vetService;
 
     public DataLoader(OwnerService ownerService, VetService vetService) {
         this.ownerService = ownerService;
@@ -30,13 +30,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("George");
         owner1.setLastName("Washington");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Michale");
         owner2.setLastName("Watson");
         ownerService.save(owner2);
@@ -44,13 +42,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Lara");
         vet1.setLastName("Croft");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Dominica");
         vet2.setLastName("Proot");
         vetService.save(vet2);
