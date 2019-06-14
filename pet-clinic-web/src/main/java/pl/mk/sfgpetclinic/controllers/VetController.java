4d9fe33c7920b561eeead47d5c27a162b98a7b0a
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-14 11:28:19 +0200

    owners and vets listed

--- pet-clinic-web/src/main/java/pl/mk/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/mk/sfgpetclinic/controllers/VetController.java
@@ -1,7 +1,9 @@
 package pl.mk.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mk.sfgpetclinic.services.VetService;
 
 /**
  * @author Mariusz Kowalczuk
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets/index", "vets", "vets/index.html", "vets/"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
 
